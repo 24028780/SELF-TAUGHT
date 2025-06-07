8c98178e8df6e8229c2b46c7d1285a4ca9b590ef
polite <politendzivalelo@gmail.com>
2025-06-07 22:02:44 +0200

    First commit

---
+++ src/main/java/com/mycompany/methods/Methods.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.methods;
+
+/**
+ *
+ * @author polit
+ */
+public class Methods {
+//BLOCK OF  REUSABLE CODE THAT IS EXECUTED WHENEVER IT'S CALLED UPON
+    public static void main(String[] args) {
+        
+        String SURNAME="Mulaudzi";
+      Polite(SURNAME); 
+    }
+    static void Polite(String SURNAME){
+        System.out.println("Hi NDZIVA  " + SURNAME);
+}
+}
+
